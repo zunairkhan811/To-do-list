6cf5fad331877b15d0065ba9f02c69f8d1ce43f4
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-03-28 20:45:30 +0500

    Removed linter errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import './styles.css';
+
 const taskList = document.querySelector('#task-list');
 
-let arr = [{
-    description: 'wash the dishes',
-    completed: 'false',
-    index: '0'
-},{
-    description: 'complete to do list project',
-    completed: 'false',
-    index: '1'
+const arr = [{
+  description: 'wash the dishes',
+  completed: 'false',
+  index: '0',
+}, {
+  description: 'complete to do list project',
+  completed: 'false',
+  index: '1',
 }];
 
-arr.forEach((items)=>{
-    taskList.innerHTML += `
-    <div class="tasks"><input type="checkbox" name="first" value="first" id="first"><label class="tasks-text" for="first">${items.description}</label><span class="tasks-icon"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></span></div>`
-})
+arr.forEach((items) => {
+  taskList.innerHTML += `
+    <div class="tasks"><input type="checkbox" name="first" value="first" id="first"><label class="tasks-text" for="first">${items.description}</label><span class="tasks-icon"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></span></div>`;
+});
