ec908a157825b9a8862e0484e93355a70e2bd8c4
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-05-21 19:43:59 +0500

    increase height of textarea as text increases

--- src/modules/displayTodo.js
+++ src/modules/displayTodo.js
@@ -1,15 +1,17 @@
+import increaseHeight from "./increaseTextArea";
 const displayTodo = (arr)=>{
 const taskList = document.querySelector('#task-list');
 console.log(taskList);
 arr.map((item)=>{
     taskList.innerHTML +=`
     <div id=${item.id} class="task-item">
-        <input type="checkbox" ${item.completed ? checked : notchecked }>
-        <textarea name="title" id="title" cols="30" >${item.title}</textarea>
+        <input type="checkbox" ${item.completed ? 'checked' : '!checked' } >
+        <textarea name="title" id="title" rows='1' cols="30" >${item.title}</textarea>
         <i class="fa fa-ellipsis-v"></i>
         <i class="fa fa-trash-o"></i>
     </div>
     `
+    increaseHeight();
 })
 }
 
